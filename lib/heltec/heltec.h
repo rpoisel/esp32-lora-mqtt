7e7ffb1fe2853fca0b6537d35e9d27845cd18f4b
Rainer Poisel <rainer.poisel@gmail.com>
2019-12-23 19:27:01 +0100

    Initial code base

---
+++ lib/heltec/heltec.h
@@ -0,0 +1,54 @@
+
+
+#ifndef _HELTEC_H_
+#define _HELTEC_H_
+
+#if defined(ESP32)
+
+#include <Arduino.h>
+#if defined( WIFI_Kit_32 ) || defined( WIFI_LoRa_32 ) || defined( WIFI_LoRa_32_V2 ) || defined( Wireless_Stick )
+#  include <Wire.h>
+#  include "SSD1306Wire.h"
+#endif
+
+#if defined( WIFI_LoRa_32 ) || defined( WIFI_LoRa_32_V2 ) || defined( Wireless_Stick ) || defined( Wireless_Stick_Lite )
+#  include <SPI.h>
+#  include "LoRa.h"
+#endif
+
+constexpr uint8_t LED = 25;
+constexpr uint8_t Vext = 21;
+constexpr uint8_t SDA_OLED = 4;
+constexpr uint8_t SCL_OLED = 15;
+constexpr uint8_t RST_OLED = 16;
+constexpr uint8_t RST_LoRa = 14;
+constexpr uint8_t DIO0 = 34;
+
+class Heltec_ESP32 {
+
+ public:
+    Heltec_ESP32();
+	~Heltec_ESP32();
+
+    void begin(bool DisplayEnable=true, bool LoRaEnable=true, bool SerialEnable=true, bool PABOOST=true, long BAND=470E6);
+#if defined( WIFI_LoRa_32 ) || defined( WIFI_LoRa_32_V2 ) || defined( Wireless_Stick ) || defined( Wireless_Stick_Lite )
+    LoRaClass LoRa;
+#endif
+
+#if defined( WIFI_Kit_32 ) || defined( WIFI_LoRa_32 ) || defined( WIFI_LoRa_32_V2 ) || defined( Wireless_Stick )
+    SSD1306Wire *display;
+#endif
+
+/*wifi kit 32 and WiFi LoRa 32(V1) do not have vext*/
+    void VextON(void);
+    void VextOFF(void);
+};
+
+extern Heltec_ESP32 Heltec;
+
+#else
+#error ¡°This library only supports boards with ESP32 processor.¡±
+#endif
+
+
+#endif
