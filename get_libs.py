2c6a1e3bdcce3e27b7ecfb2909b372b2701ef76c
Rainer Poisel <rainer.poisel@gmail.com>
2019-12-31 19:28:09 +0100

    Pulling external Crypto library

---
+++ get_libs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+Import("env")
+
+EXTLIBS_DIR = env.subst("$PROJECT_DIR/extlib")
+CRYPTOLIB_DIR = EXTLIBS_DIR + os.sep + "Crypto"
+
+if not os.path.exists(EXTLIBS_DIR):
+    print("Cloning Arduino Crypto lib ...")
+    env.Execute(
+        "git clone --depth 32 https://github.com/rweather/arduinolibs " + CRYPTOLIB_DIR)
+else:
+    print("Checking for updates of Crypto lib ...")
+    env.Execute("git --work-tree=" + CRYPTOLIB_DIR + " --git-dir=" +
+                CRYPTOLIB_DIR + os.sep + ".git pull origin master --depth 32")
