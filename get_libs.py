45c40cd1371d4adeaad428607e281c767f2fc845
Rainer Poisel <rainer.poisel@gmail.com>
2020-01-02 19:57:54 +0100

    VS Code project data

--- get_libs.py
+++ get_libs.py
@@ -8,11 +8,11 @@ Import("env")
 EXTLIBS_DIR = env.subst("$PROJECT_DIR/extlib")
 CRYPTOLIB_DIR = EXTLIBS_DIR + os.sep + "Crypto"
 
-if not os.path.exists(EXTLIBS_DIR):
+if not os.path.exists(CRYPTOLIB_DIR):
     print("Cloning Arduino Crypto lib ...")
     env.Execute(
-        "git clone --depth 32 https://github.com/rweather/arduinolibs " + CRYPTOLIB_DIR)
+        "git clone --depth 32 -b valid_json https://github.com/rpoisel/arduinolibs.git " + CRYPTOLIB_DIR)
 else:
     print("Checking for updates of Crypto lib ...")
     env.Execute("git --work-tree=" + CRYPTOLIB_DIR + " --git-dir=" +
-                CRYPTOLIB_DIR + os.sep + ".git pull origin master --depth 32")
+                CRYPTOLIB_DIR + os.sep + ".git pull origin valid_json --depth 32")
