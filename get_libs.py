01e7d6fe61124e1351084e7728827299c411eb72
Rainer Poisel <rainer.poisel@gmail.com>
2020-01-03 19:25:14 +0100

    Using original crypto library

--- get_libs.py
+++ get_libs.py
@@ -11,8 +11,8 @@ CRYPTOLIB_DIR = EXTLIBS_DIR + os.sep + "Crypto"
 if not os.path.exists(CRYPTOLIB_DIR):
     print("Cloning Arduino Crypto lib ...")
     env.Execute(
-        "git clone --depth 32 -b valid_json https://github.com/rpoisel/arduinolibs.git " + CRYPTOLIB_DIR)
+        "git clone --depth 32 https://github.com/rweather/arduinolibs.git " + CRYPTOLIB_DIR)
 else:
     print("Checking for updates of Crypto lib ...")
     env.Execute("git --work-tree=" + CRYPTOLIB_DIR + " --git-dir=" +
-                CRYPTOLIB_DIR + os.sep + ".git pull origin valid_json --depth 32")
+                CRYPTOLIB_DIR + os.sep + ".git pull origin master --depth 32")
